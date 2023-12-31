a9d717225eeda7a7993304bc887cfeea46c65102
Arulrajgopal <122370244+Arulrajgopal@users.noreply.github.com>
2023-12-31 11:48:45 +0530

    Update create_dataframe.py

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,1 +1,15 @@
 
+data = [
+    ("John", 28, "New York"),
+    ("Jane", 24, "San Francisco"),
+    ("Bob", 22, "Los Angeles"),
+    ("Alice", 30, "Chicago")
+]
+
+Columns = ["Name", "Age","City" ]
+
+
+df = spark.createDataFrame(data, columns)
+
+
+df.show()
