d6a122e2b49426644cda973d06c56e44e0e4a837
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2024-01-10 12:40:21 +0530

    Create pyspark_install_python.py

---
+++ pyspark_install_python.py
@@ -0,0 +1,9 @@
+!pip install pyspark
+
+from pyspark.sql import SparkSession
+
+#create spark session
+spark= SparkSession.builder.appName('mysparksession').getOrCreate()
+
+#create spark context
+sc = spark.sparkContext
