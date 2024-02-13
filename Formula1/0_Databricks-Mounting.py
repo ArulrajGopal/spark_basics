2c49c795a3e79b53c1540c6a427b07a220a04f5c
ArulrajGopal <ArulrajGopal>
2024-02-13 15:47:15 +0000

    mount point updated

--- Formula1/0_Databricks-Mounting.py
+++ Formula1/0_Databricks-Mounting.py
@@ -1,7 +1,11 @@
 # Databricks notebook source
-storage_account_name = "teststorageaccount23423"  
-accountkey= "vWM4wogy4NzlOJ+1Lbci2OT/Bza2n1Lv8XC7+XvfFUiNsN1LEb0AobHKKA+y5b4jkQ9GlIYXPGCz+AStdKJtng=="  
-container_name = "input"
+dbutils.fs.ls("dbfs:/")
+
+# COMMAND ----------
+
+storage_account_name = "sparkdemostorageaccount"  
+accountkey= "ha8V0wsWYgj55/w7EMvwczPxDmcLXkKkkSN0+w0bMu9r/mDn0X4olKKim4Vw29S3Wgu3EA3UKwJD+AStVfFw7g=="  
+container_name = "raw"
 fullname = "fs.azure.account.key." +storage_account_name+ ".blob.core.windows.net"
 
 
@@ -9,3 +13,19 @@ dbutils.fs.mount(
   source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net", 
   mount_point =f"/mnt/{container_name}", 
   extra_configs = {fullname : accountkey}) 
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/")
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/mnt/"))
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/mnt/raw/"))
+
+# COMMAND ----------
+
+
