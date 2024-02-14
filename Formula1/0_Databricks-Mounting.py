f1fa41c48fc3d1c622513f082d483f24d266dc04
ArulrajGopal <ArulrajGopal>
2024-02-14 06:17:09 +0000

    update

--- Formula1/0_Databricks-Mounting.py
+++ Formula1/0_Databricks-Mounting.py
@@ -24,28 +24,16 @@ display(dbutils.fs.ls("/mnt/raw"))
 
 # COMMAND ----------
 
-storage_account_name = "sparkdemostorageaccount"  
-accountkey= "ha8V0wsWYgj55/w7EMvwczPxDmcLXkKkkSN0+w0bMu9r/mDn0X4olKKim4Vw29S3Wgu3EA3UKwJD+AStVfFw7g=="  
-container_name = "stage"
-fullname = "fs.azure.account.key." +storage_account_name+ ".blob.core.windows.net"
-
-
 dbutils.fs.mount(  
-  source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net", 
-  mount_point =f"/mnt/{container_name}", 
+  source = f"wasbs://stage@{storage_account_name}.blob.core.windows.net", 
+  mount_point =f"/mnt/stage", 
   extra_configs = {fullname : accountkey}) 
 
 # COMMAND ----------
 
-storage_account_name = "sparkdemostorageaccount"  
-accountkey= "ha8V0wsWYgj55/w7EMvwczPxDmcLXkKkkSN0+w0bMu9r/mDn0X4olKKim4Vw29S3Wgu3EA3UKwJD+AStVfFw7g=="  
-container_name = "curated"
-fullname = "fs.azure.account.key." +storage_account_name+ ".blob.core.windows.net"
-
-
 dbutils.fs.mount(  
-  source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net", 
-  mount_point =f"/mnt/{container_name}", 
+  source = f"wasbs://curated@{storage_account_name}.blob.core.windows.net", 
+  mount_point =f"/mnt/curated", 
   extra_configs = {fullname : accountkey}) 
 
 # COMMAND ----------
